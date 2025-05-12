6800cf4610f08d2381bc8db397f94343dd9ed528
pedrodelfilho <40432255+pedrodelfilho@users.noreply.github.com>
2025-05-11 22:31:45 -0300

    ajuste url

--- Application/Services/Handler/Handler.cs
+++ Application/Services/Handler/Handler.cs
@@ -7,7 +7,7 @@ namespace Application.Services.Handler
     {
         public static string Deploy(string idUsuario, string token, string resource)
         {
-            var uriBuilder = new UriBuilder("https", "localhost", 44300);
+            var uriBuilder = new UriBuilder("https://frontendd-g2checd7czhghpf5.brazilsouth-01.azurewebsites.net/");
             uriBuilder.Path = resource;
             var query = HttpUtility.ParseQueryString(uriBuilder.Query);
             query["token"] = token;
